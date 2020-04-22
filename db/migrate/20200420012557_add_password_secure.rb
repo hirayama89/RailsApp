12b5a2e5556f1b168b950eb7d31d80380c375d8e
shl80525 <ks.hirayama@hotmail.com>
2020-04-22 13:42:52 +0900

    migrate update

--- db/migrate/20200420012557_add_password_secure.rb
+++
@@ -1,6 +0,0 @@
-class AddPasswordSecure < ActiveRecord::Migration[5.2]
-  def change
-    add_column :users, :password_digest, :string
-    remove_column :users, :password, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_20_012557) do
+ActiveRecord::Schema.define(version: 2020_04_18_233622) do
 
   create_table "likes", force: :cascade do |t|
     t.integer "user_id"
@@ -21,18 +21,18 @@ ActiveRecord::Schema.define(version: 2020_04_20_012557) do
 
   create_table "posts", force: :cascade do |t|
     t.text "content"
+    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.string "image_name"
     t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
